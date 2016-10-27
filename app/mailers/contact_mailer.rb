15094b87775ed274a9eec63d10201a25555bed32
Kevin Hobbs <kevin21921@gmail.com>
2016-10-27 19:53:45 -0400

    Added additional email for default mailer option.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,6 @@
 class ContactMailer < ActionMailer::Base
-	default to: 'jbryanvaughn@yahoo.com'
+	# default to: 'jbryanvaughn@yahoo.com'
+	default to: 'kevin21921@gmail.com'
 
 	def contact_email(contact_name, email, body)
 
